86540b45628523ad21383d3149011edb7b86a5cd
Alyssa Doerscher <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-09-06 14:25:58 -0500

    results styling

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ $(document).ready(function() {
 
   $('#searchButton').on("click", function() {
     var searchTerm = $('#searchField').val();
-    var queryUrl = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&limit=10&api_key=dc6zaTOxFJmzC"
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q="+searchTerm+"&limit=12&api_key=dc6zaTOxFJmzC"
 
     if(firstSeach) {
       $('.main').animate({top: '20px'});
+      $('.form-control').css({
+        "height": "32px",
+        "font-size": "14px"
+      });
       $('#results, #pastSearches').fadeIn(700);
       $('#suggestions').addClass('disable');
       firstSeach = false;
+    } else {
+      $('#results').empty();
     }
 
     $.ajax({
       url: queryUrl,
       method: 'GET'
     }).done(function(response) {
-      for(var i = 0; i < 10; i++) {
+      for(var i = 0; i < 12; i++) {
         var fixedImage = response.data[i].images.fixed_height_still.url;
         $('#results').append('<div>');
         $('#results > div').addClass('gif');
